e5aa50665c220fd2f21a33d48a46fd7a1708edc7
loipv03 <phanvanloi1522003@gmail.com>
2023-07-19 22:13:42 +0700

    update

--- main.js
+++ main.js
@@ -1,66 +1,61 @@
 const changeHeight = () => {
-    let listCate = document.querySelector('.list__categories');
-    let iconCateHide = document.querySelector('.adjust');
-    document.querySelector('.hero__categories__all').addEventListener('click', () => {
-        listCate.classList.toggle('list__categories__Hide');
-        iconCateHide.querySelector('i').classList.toggle('rotate');
+  let listCate = document.querySelector(".list__categories");
+  let iconCateHide = document.querySelector(".adjust");
+  document
+    .querySelector(".hero__categories__all")
+    .addEventListener("click", () => {
+      listCate.classList.toggle("list__categories__Hide");
+      iconCateHide.querySelector("i").classList.toggle("rotate");
     });
-}
-changeHeight()
+};
+changeHeight();
 
 const slideProduct = () => {
-    try {
-        let interval;
-        let currentSlide = 0;
-        const indicators = document.querySelectorAll(".indicator");
-        const slide = document.querySelector('.slide');
-        const btnNext = document.querySelector('.btn-next');
-        const btnPre = document.querySelector('.btn-pre');
+  try {
+    let interval;
+    let currentSlide = 0;
+    const indicators = document.querySelectorAll(".indicator");
+    const slide = document.querySelector(".slide");
+    const btnNext = document.querySelector(".btn-next");
+    const btnPre = document.querySelector(".btn-pre");
 
-        function startInterval() {
-            interval = setInterval(() => {
-                moveSlide('next')
-            }, 2500);
-        }
-        startInterval();
-
-        function moveSlide(move) {
-            let imgItem = document.querySelectorAll('.item')
-            clearInterval(interval)
-            move === 'next' ?
-                (
-                    slide.appendChild(imgItem[0]),
-                    updateIndicator('next')
-                )
-                :
-                (
-                    slide.prepend(imgItem[imgItem.length - 1]),
-                    updateIndicator('pre')
-                )
-            startInterval()
-        }
-
-        function updateIndicator(direction) {
-            indicators[currentSlide].classList.remove("active");
-            if (direction === 'next') {
-                currentSlide = (currentSlide + 1) % indicators.length;
-            } else {
-                currentSlide = (currentSlide - 1 + indicators.length) % indicators.length;
-            }
-            indicators[currentSlide].classList.add("active");
-        }
-
-        btnNext.addEventListener('click', () => {
-            moveSlide('next')
-        });
-
-        btnPre.addEventListener('click', () => {
-            moveSlide('pre')
-        });
-
-    } catch (error) {
-        console.log(error.message);
+    function startInterval() {
+      interval = setInterval(() => {
+        moveSlide("next");
+      }, 2500);
     }
-}
+    startInterval();
 
-slideProduct()
+    function moveSlide(move) {
+      let imgItem = document.querySelectorAll(".item");
+      clearInterval(interval);
+      move === "next"
+        ? (slide.appendChild(imgItem[0]), updateIndicator("next"))
+        : (slide.prepend(imgItem[imgItem.length - 1]), updateIndicator("pre"));
+      startInterval();
+    }
+
+    function updateIndicator(direction) {
+      indicators[currentSlide].classList.remove("active");
+      if (direction === "next") {
+        currentSlide = (currentSlide + 1) % indicators.length;
+      } else {
+        currentSlide =
+          (currentSlide - 1 + indicators.length) % indicators.length;
+      }
+      indicators[currentSlide].classList.add("active");
+    }
+
+    btnNext.addEventListener("click", () => {
+      moveSlide("next");
+    });
+
+    btnPre.addEventListener("click", () => {
+      moveSlide("pre");
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+slideProduct();
